57069892d071214f6b94d4c5868cd90b350f6a1f
hiroko-ino <los73.butterfly.1251.12.15@gmail.com>
2022-01-11 22:31:10 +0900

    プログレスバー、仮

---
+++ components/Progress.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="absolute bottom-5 left-0 right-0 flex justify-center">
+        <div class="w-50 relative h-1 bg-green-500/50 rounded-full overflow-hidden">
+            <span class="absolute h-full bg-gray-500 transition-all duration-300" :style="style"></span>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    export default {
+        name: "Progress",
+        props: {
+            current: {
+                type: Number,
+                default: 0,
+                required: true
+            }
+        },
+        computed: {
+            style() {
+                return {
+                    width: `${100 / 7}%`,
+                    left: `${(100 / 7) * (this.current - 2)}%`,
+                }
+            }
+        }
+    };
+</script>
---
+++ global-bottom.vue
@@ -0,0 +1,4 @@
+<!-- hide the footer from Page 4 -->
+<template>
+    <Progress v-if="$slidev.nav.currentPage !== 1" v-bind:current="$slidev.nav.currentPage" />
+</template>
